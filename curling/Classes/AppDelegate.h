a55b19c00e9db4a03907f8c38ab1c3dfb7413848
uuudbx <uuudbx@gmail.com>
2014-05-29 18:30:41 +0800

    Initial Commit

---
+++ curling/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  curling
+//
+//  Created by BravoDavid on 29/5/14.
+//  Copyright BravoDavid 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ curling/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  curling
+//
+//  Created by BravoDavid on 29/5/14.
+//  Copyright BravoDavid 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
